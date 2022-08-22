5c20c2ba4ecdb58cc5e66e44fe62bdb5bcd9144f
lizkes <erfan2333@gmail.com>
2022-08-22 13:43:56 +0800

    更新

--- src/lib/crypto.rs
+++ src/lib/crypto.rs
@@ -1,14 +1,13 @@
-pub fn encrypt_message(message: &str) -> String {
-    use aes::Aes256;
-    use block_modes::{block_padding::Pkcs7, BlockMode, Cbc};
+use aes::cipher::{block_padding::Pkcs7, BlockEncryptMut, KeyIvInit};
 
-    type Aes256Cbc = Cbc<Aes256, Pkcs7>;
+pub fn encrypt_message(message: &str) -> String {
+    type Aes256CbcEnc = cbc::Encryptor<aes::Aes256>;
 
     let key: Vec<u8> = base64::decode("DP/B868Op9Ataw0l2YGtaS822jt26XWv7e3vMVa5zFI=").unwrap();
     let iv: Vec<u8> = base64::decode("3rqBYyUB02E5HLOCI2i/2A==").unwrap();
-    let cipher = Aes256Cbc::new_from_slices(key.as_slice(), iv.as_slice()).unwrap();
-    let encrypted_content = cipher.encrypt_vec(message.as_bytes());
-    let content = base64::encode(encrypted_content);
+    let encryptor = Aes256CbcEnc::new_from_slices(key.as_slice(), iv.as_slice()).unwrap();
+    let ciphertext = encryptor.encrypt_padded_vec_mut::<Pkcs7>(message.as_bytes());
+    let content = base64::encode(ciphertext);
 
     return content;
 }
